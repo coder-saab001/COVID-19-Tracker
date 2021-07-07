dd3e8971214842785d62264b7244f2d273ea741d
coder-saab001 <76659019+coder-saab001@users.noreply.github.com>
2021-07-07 22:54:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,11 +8,9 @@ function updateMap(){
             longitude=element.longitude;
             cases=element.confirmed;
             if(cases>10000000) color=255;
-            else if(cases>7000000) color=200;
-            else if(cases>4000000) color=160;
-            else if(cases>1000000) color=140;
-            else if(cases>100000) color=100;
-            color=0+(cases/100000)*100;
+            else if(cases>1000000) color=200;
+            else if(cases>100000) color=170;
+            else color=0+(cases/100000)*170;
 
             const tex=`<div class="pop"><pre><h1>${element.location}</h1>
 <span id="confirmed">Confirmed: ${cases.toLocaleString()}</span>
